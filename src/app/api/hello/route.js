44597209cbcad9e4cc4a3f3bbe35bbec5ee078b1
Long Tran <tranxuanlonga555@gmail.com>
2025-02-20 23:32:50 +0700

    fix cors

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,3 +1,15 @@
+export async function OPTIONS() {
+    return new Response(null, {
+        status: 204, // No Content
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Max-Age": "86400" // Cache preflight request
+        }
+    });
+}
+
 export async function GET() {
     return Response.json({
         data: [
