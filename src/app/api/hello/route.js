c4b1c5986e9a4c9ff99da70feadf5453c37be749
Long Tran <tranxuanlonga555@gmail.com>
2025-02-20 23:21:38 +0700

    fix json

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -8,7 +8,7 @@ export async function GET() {
                 from: "2024-12-20T09:30:35Z",
                 to: "2024-12-20T10:30:35Z",
                 location: "Park Hyatt Saigon",
-                ticketPrice: 0,
+                ticketPrice: 10,
                 host: "Vietnam Private Capital Agency (VPCA)",
                 followers: 29,
                 image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
@@ -32,7 +32,7 @@ export async function GET() {
                 from: "2024-12-20T2:30:35Z",
                 to: "2024-12-20T3:30:35Z",
                 location: "Park Hyatt Saigon",
-                ticketPrice: 0,
+                ticketPrice: 20,
                 host: "Vietnam Private Capital Agency (VPCA)",
                 followers: 29,
                 image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
@@ -44,7 +44,7 @@ export async function GET() {
                 from: "2024-12-23T18:29:35Z",
                 to: "2024-12-23T18:50:35Z",
                 location: "Park Hyatt Saigon",
-                ticketPrice: 0,
+                ticketPrice: 50,
                 host: "Vietnam Private Capital Agency (VPCA)",
                 followers: 29,
                 image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
