d58705aea7e24ccc6b462a4a2d4f7ae8433bfc9d
Long Tran <tranxuanlonga555@gmail.com>
2025-02-20 23:15:24 +0700

    fix json

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -3,8 +3,10 @@ export async function GET() {
         data: [
             {
                 id: 1,
-                name: "LUNCH & LEARN",
-                time: "2024-12-23T18:29:35Z",
+                type: "appointment",
+                name: "First Session with Alex Stan",
+                from: "2024-12-20T09:30:35Z",
+                to: "2024-12-20T10:30:35Z",
                 location: "Park Hyatt Saigon",
                 ticketPrice: 0,
                 host: "Vietnam Private Capital Agency (VPCA)",
@@ -13,8 +15,46 @@ export async function GET() {
             },
             {
                 id: 2,
+                type: "event",
+                name: "Webinar: How to cope with trauma in profressional life",
+                from: "2024-12-20T09:30:35Z",
+                to: "2024-12-20T11:00:35Z",
+                location: "Park Hyatt Saigon",
+                ticketPrice: 0,
+                host: "Vietnam Private Capital Agency (VPCA)",
+                followers: 29,
+                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
+            },
+            {
+                id: 3,
+                type: "appointment",
+                name: "First Session with Alex Stan",
+                from: "2024-12-20T2:30:35Z",
+                to: "2024-12-20T3:30:35Z",
+                location: "Park Hyatt Saigon",
+                ticketPrice: 0,
+                host: "Vietnam Private Capital Agency (VPCA)",
+                followers: 29,
+                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
+            },
+            {
+                id: 4,
+                type: "event",
+                name: "LUNCH & LEARN",
+                from: "2024-12-23T18:29:35Z",
+                to: "2024-12-23T18:50:35Z",
+                location: "Park Hyatt Saigon",
+                ticketPrice: 0,
+                host: "Vietnam Private Capital Agency (VPCA)",
+                followers: 29,
+                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
+            },
+            {
+                id: 5,
+                type: "event",
                 name: "She Is Women's Conference 2025 - BLOOM",
-                time: "2024-12-04T12:58:46Z",
+                time: "2024-12-04T03:00:46Z",
+                time: "2024-12-04T4:00:46Z",
                 location: "River Church",
                 ticketPrice: 16,
                 host: "River Church",
@@ -22,9 +62,11 @@ export async function GET() {
                 image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
             },
             {
-                id: 3,
+                id: 6,
+                type: "event",
                 name: "2025 EB-5 & Global Immigration Expo Vietnam",
-                time: "2024-11-27T18:29:50Z",
+                from: "2024-11-27T19:30:50Z",
+                to: "2024-11-27T20:00:50Z",
                 location: "The Reverie Saigon",
                 ticketPrice: 3405.8,
                 host: "Uglobal Immigration Magazine/EB5 Investors Magazine",
@@ -32,9 +74,11 @@ export async function GET() {
                 image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd"
             },
             {
-                id: 4,
+                id: 7,
+                type: "event",
                 name: "LUNCH & LEARN",
-                time: "2024-12-23T18:29:35Z",
+                from: "2024-12-23T07:00:35Z",
+                to: "2024-12-23T07:30:35Z",
                 location: "Park Hyatt Saigon",
                 ticketPrice: 0,
                 host: "Vietnam Private Capital Agency (VPCA)",
