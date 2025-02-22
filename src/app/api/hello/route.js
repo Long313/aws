d896c2331a9b05e754959c439ff22f13855baf79
Long Tran <tranxuanlonga555@gmail.com>
2025-02-22 13:20:17 +0700

    fix json 5

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -31,95 +31,95 @@ export async function GET() {
                     age: 29,
                     address: "District 7, Ho Chi Minh city"
                 }            },
-            {
-                id: 2,
-                type: "event",
-                name: "She Is Women's Conference 2025 - BLOOM",
-                from: "2025-02-22T09:30:35Z",
-                to: "2025-02-23T11:00:35Z",
-                location: "River Church",
-                ticketPrice: 16,
-                host: "River Church",
-                followers: 56,
-                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                client: {
-                    avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                    name: "Nguyen Van A",
-                    age: 29,
-                    address: "District 7, Ho Chi Minh city"
-                }
-            },
-            {
-                id: 3,
-                type: "appointment",
-                name: "2025 EB-5 & Global Immigration Expo Vietnam",
-                from: "2025-02-22T09:30:35Z",
-                to: "2025-02-23T11:00:35Z",
-                location: "The Reverie Saigon",
-                ticketPrice: 3405.8,
-                host: "Uglobal Immigration Magazine/EB5 Investors Magazine",
-                followers: 1000,
-                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                client: {
-                    avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                    name: "Nguyen Van A",
-                    age: 29,
-                    address: "District 7, Ho Chi Minh city"
-                }
-            },
-            {
-                id: 4,
-                type: "event",
-                name: "LUNCH & LEARN",
-                from: "2024-12-23T07:00:35Z",
-                to: "2024-12-23T07:30:35Z",
-                location: "Park Hyatt Saigon",
-                ticketPrice: 0,
-                host: "Vietnam Private Capital Agency (VPCA)",
-                followers: 29,
-                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                client: {
-                    avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                    name: "Nguyen Van A",
-                    age: 29,
-                    address: "District 7, Ho Chi Minh city"
-                }
-            },
-            {
-                id: 5,
-                type: "appointment",
-                name: "First Session with Alex Stan",
-                from: "2025-02-23T09:30:35Z",
-                to: "2025-02-23T10:30:35Z",
-                location: "Park Hyatt Saigon",
-                ticketPrice: 10,
-                host: "Vietnam Private Capital Agency (VPCA)",
-                followers: 29,
-                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                client: {
-                    avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                    name: "Nguyen Van A",
-                    age: 29,
-                    address: "District 7, Ho Chi Minh city"
-                }
-            },
-            {
-                id: 6,
-                type: "event",
-                name: "Webinar: How to cope with trauma in profressional life",
-                from: "2025-02-22T09:30:35Z",
-                to: "2025-02-22T11:00:35Z",
-                location: "Park Hyatt Saigon",
-                ticketPrice: 0,
-                host: "Vietnam Private Capital Agency (VPCA)",
-                followers: 29,
-                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                client: {
-                    avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
-                    name: "Nguyen Van A",
-                    age: 29,
-                    address: "District 7, Ho Chi Minh city"
-                }            },
+            // {
+            //     id: 2,
+            //     type: "event",
+            //     name: "She Is Women's Conference 2025 - BLOOM",
+            //     from: "2025-02-22T09:30:35Z",
+            //     to: "2025-02-23T11:00:35Z",
+            //     location: "River Church",
+            //     ticketPrice: 16,
+            //     host: "River Church",
+            //     followers: 56,
+            //     image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //     client: {
+            //         avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //         name: "Nguyen Van A",
+            //         age: 29,
+            //         address: "District 7, Ho Chi Minh city"
+            //     }
+            // },
+            // {
+            //     id: 3,
+            //     type: "appointment",
+            //     name: "2025 EB-5 & Global Immigration Expo Vietnam",
+            //     from: "2025-02-22T09:30:35Z",
+            //     to: "2025-02-23T11:00:35Z",
+            //     location: "The Reverie Saigon",
+            //     ticketPrice: 3405.8,
+            //     host: "Uglobal Immigration Magazine/EB5 Investors Magazine",
+            //     followers: 1000,
+            //     image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //     client: {
+            //         avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //         name: "Nguyen Van A",
+            //         age: 29,
+            //         address: "District 7, Ho Chi Minh city"
+            //     }
+            // },
+            // {
+            //     id: 4,
+            //     type: "event",
+            //     name: "LUNCH & LEARN",
+            //     from: "2024-12-23T07:00:35Z",
+            //     to: "2024-12-23T07:30:35Z",
+            //     location: "Park Hyatt Saigon",
+            //     ticketPrice: 0,
+            //     host: "Vietnam Private Capital Agency (VPCA)",
+            //     followers: 29,
+            //     image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //     client: {
+            //         avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //         name: "Nguyen Van A",
+            //         age: 29,
+            //         address: "District 7, Ho Chi Minh city"
+            //     }
+            // },
+            // {
+            //     id: 5,
+            //     type: "appointment",
+            //     name: "First Session with Alex Stan",
+            //     from: "2025-02-23T09:30:35Z",
+            //     to: "2025-02-23T10:30:35Z",
+            //     location: "Park Hyatt Saigon",
+            //     ticketPrice: 10,
+            //     host: "Vietnam Private Capital Agency (VPCA)",
+            //     followers: 29,
+            //     image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //     client: {
+            //         avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //         name: "Nguyen Van A",
+            //         age: 29,
+            //         address: "District 7, Ho Chi Minh city"
+            //     }
+            // },
+            // {
+            //     id: 6,
+            //     type: "event",
+            //     name: "Webinar: How to cope with trauma in profressional life",
+            //     from: "2025-02-22T09:30:35Z",
+            //     to: "2025-02-22T11:00:35Z",
+            //     location: "Park Hyatt Saigon",
+            //     ticketPrice: 0,
+            //     host: "Vietnam Private Capital Agency (VPCA)",
+            //     followers: 29,
+            //     image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //     client: {
+            //         avatar: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F918826313%2F188149727845%2F1%2Foriginal.20241217-123211?w=400&auto=format%2Ccompress&q=75&sharp=10&s=38c457d0fbd2274456bf4688975ff7cd",
+            //         name: "Nguyen Van A",
+            //         age: 29,
+            //         address: "District 7, Ho Chi Minh city"
+            //     }            },
             // {
             //     id: 7,
             //     type: "appointment",
