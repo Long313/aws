c5ca454f2de0baa22e14d54c41d62cd88357c437
Long Tran <tranxuanlonga555@gmail.com>
2025-02-22 19:03:42 +0700

    fix 1

--- src/app/api/all/route.js
+++ src/app/api/all/route.js
@@ -1,11 +1,11 @@
 export async function OPTIONS() {
     return new Response(null, {
-        status: 204, // No Content
+        status: 204, 
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Max-Age": "86400" // Cache preflight request
+            "Access-Control-Max-Age": "86400"
         }
     });
 }
